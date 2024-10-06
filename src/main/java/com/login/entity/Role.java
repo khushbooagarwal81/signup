aab36c3004bb715b13db9b3f30701c2170d496dd
khushbooagarwal81 <ka8138807@gmail.com>
2024-10-06 17:09:38 +0530

    Project added

---
+++ src/main/java/com/login/entity/Role.java
@@ -0,0 +1,22 @@
+package com.login.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import jakarta.persistence.*;
+
+
+@Setter
+@Getter
+@Entity
+@Table(name = "roles")
+public class Role {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private long id;
+	
+	@Column(length = 60)
+	private String name;
+
+}
---
+++ src/main/java/com/login/payload/SignUpDto.java
@@ -0,0 +1,12 @@
+package com.login.payload;
+
+import lombok.Data;
+
+@Data
+public class SignUpDto {
+
+	private String name;
+	private String username;
+	private String email;
+	private String password;
+}
